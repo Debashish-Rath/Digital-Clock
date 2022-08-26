88056235f653a8ed5bd8df60e4296164942721e9
DebashishRath9090 <86132069+DebashishRath9090@users.noreply.github.com>
2022-08-26 14:58:46 +0530

    fixed the rotation position of hour_dot of hour_circle

--- js/script.js
+++ js/script.js
@@ -34,15 +34,15 @@ setInterval(() => {
     seconds.innerHTML = s + '<br><span>Seconds</span>';
     // ampm.innerHTML = am;
 
-    hh.style.strokeDashoffset = 440 - (440 * h) / 12;
-    // 12 Hrs Clock
+    hh.style.strokeDashoffset = 440 - (440 * h) / 24;
+    // / 24 (for 24Hrs Clock) , / 12 (for 12hrs Clock format)
     mm.style.strokeDashoffset = 440 - (440 * m) / 60;
     // 60 Minutes
     ss.style.strokeDashoffset = 440 - (440 * s) / 60;
     // 60 Seconds
 
-    hr_dot.style.transform = `rotate(${h * 30}deg)`;
-    // 360 / 12 = 30
+    hr_dot.style.transform = `rotate(${h * 15}deg)`;
+    // 360 / 24 = 15 (for 24hrs Clock format) , 360 / 12 = 30 (for 12hrs Clock format)
     min_dot.style.transform = `rotate(${m * 6}deg)`;
     // 360 / 60 = 6
     sec_dot.style.transform = `rotate(${s * 6}deg)`;
